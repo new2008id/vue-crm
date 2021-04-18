257646933f3c30681612813d0bfd3bca7c6ba9e1
new2008id <new2008id@gmail.com>
2021-04-18 10:59:04 +0300

    123

---
+++ src/utils/messages.js
@@ -0,0 +1,7 @@
+export default {
+  'logout': 'Вы вышли из системы',
+  'login': 'Для начала войдите в систему',
+  'auth/user-not-found': 'Пользователи с таким email не существует',
+  'auth/wrong-password': 'Неверный пароль',
+  'auth/email-already-in-use': 'Email уже занят'
+}
